74d64e1d09f4199ef92ea3d655cc2e08a1bb1a74
Holli Davis <hollidavis19@gmail.com>
2021-09-07 16:09:35 -0600

    updated models

--- Models/Contractor.cs
+++ Models/Contractor.cs
@@ -3,6 +3,6 @@ namespace contracted.Models
     public class Contractor
     {
         public int Id { get; set; }
-        public int Name { get; set; }
+        public string Name { get; set; }
     }
 }
